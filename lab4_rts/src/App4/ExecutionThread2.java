09c424adc1423d0b15c2d555cc53883bc1b3be40
bosca-cristiana-marina-30322 <Bosca.Io.Cristiana@student.utcluj.ro>
2024-04-15 10:52:15 +0300

    Lab3,4

---
+++ lab4_rts/src/App4/ExecutionThread2.java
@@ -0,0 +1,31 @@
+package App4;
+
+public class ExecutionThread2 extends Thread{
+    int activity_min;
+    int activity_max;
+    ExecutionThread1 executionThread1;
+
+    public ExecutionThread2(int activity_min, int activity_max, ExecutionThread1 executionThread1) {
+        this.activity_min = activity_min;
+        this.activity_max = activity_max;
+        this.executionThread1 = executionThread1;
+    }
+
+    public void run(){
+        //s1
+        try {
+            executionThread1.waitNotify(this.getName());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(this.getName() + " - STATE 1");
+        int k = (int) Math.round(Math.random() * (activity_max - activity_min) + activity_min);
+        for (int i = 0; i < k * 100000; i++) {
+            i++;
+            i--;
+        }
+        System.out.println(this.getName() + " - STATE 2");
+        //it t not null t.join
+    }
+
+}
